85b8cd4c1fdf0c531fbf1544d71e10c9325166ec
giuhirabara <giuhirabara@gmail.com>
2023-06-12 22:39:54 -0300

    arrumando

--- jest/__tests__/Comment.test.js
+++ jest/__tests__/Comment.test.js
@@ -1,20 +1,17 @@
-import { render, fireEvent } from '@testing-library/react'
-import CommentForm from './CommentForm'
+test('renders two comments', () => {
+  render(<App />);
+  const commentInput = screen.getByTestId('comment-input');
+  const commentButton = screen.getByTestId('comment-button');
 
-test('Teste de inserção de comentários e uso do "data-testid"', () => {
-  const handleSubmit = jest.fn()
+  fireEvent.change(commentInput, { target: { value: 'Comment 1' } });
+  fireEvent.click(commentButton);
 
-  const { getByTestId } = render(<CommentForm onSubmit={handleSubmit} />)
+  fireEvent.change(commentInput, { target: { value: 'Comment 2' } });
+  fireEvent.click(commentButton);
 
-  const inputElement = getByTestId('comment-input')
+  const comment1 = screen.getByText('Comment 1');
+  const comment2 = screen.getByText('Comment 2');
 
-  fireEvent.change(inputElement, { target: { value: 'Primeiro comentário' } })
-  fireEvent.submit(getByTestId('comment-submit'))
-
-  expect(handleSubmit).toHaveBeenCalledWith('Primeiro comentário')
-
-  fireEvent.change(inputElement, { target: { value: 'Segundo comentário' } })
-  fireEvent.submit(getByTestId('comment-submit'))
-
-  expect(handleSubmit).toHaveBeenCalledWith('Segundo comentário')
-})
+  expect(comment1).toBeInTheDocument();
+  expect(comment2).toBeInTheDocument();
+});
